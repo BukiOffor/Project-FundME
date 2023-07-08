68afc5bf9c6f4b8553dc8a9151b894621a14a29b
Ebuka <ebuka2264@yahoo.com>
2023-07-08 23:47:17 +0100

    skeleton-setup

---
+++ chain/contracts/FundME.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "hardhat/console.sol";
+
+/// @title FundMe Contract by BlockChain Band
+/// @author @stephenpeter666, Fazz1998
+/// @notice This contract has not been audited and its implementations are just for learning purposes
+
+contract FundME {
+
+    //TO-DO
+    ///@dev stephenpeter666
+    ///@notice this constructor instanciates the address of the owner of this contract
+    constructor(){
+
+    }
+
+    ///@dev Fazz1998
+    ///@notice this function accepts ethers from the caller
+    function fund()public{
+
+
+    }
+
+    ///@dev stephenpeter666
+    ///@notice this function allows the owner of the contract to withdraw
+    function withdraw()external {
+
+    }
+
+    fallback()external{
+
+    }
+
+    
+}
